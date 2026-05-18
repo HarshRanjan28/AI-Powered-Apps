6f6c81d390a215696db3350f687e254bb851ee2c
Harshranjan28 <harshranjan2897@gmail.com>
2026-05-18 17:18:51 +0530

    installed tailwind for styling

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       .catch((error) => console.error("Error fetching message:", error));
   }, []);
 
-  return <p>{message}</p>;
+  return <p className="font-bold text-3xl p-4">{message}</p>;
 }
 
 export default App;
