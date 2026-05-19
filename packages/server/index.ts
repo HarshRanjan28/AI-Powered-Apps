87462b0a9ce7ce2f0efe02bb2de75b0b167d1d5b
Harshranjan28 <harshranjan2897@gmail.com>
2026-05-19 10:52:05 +0530

    Set up openrouter for model integration into our application

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { OpenRouter } from '@openrouter/sdk';
+import type { ChatResult } from '@openrouter/sdk/models';
+import z from 'zod';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
+
+const openrouter = new OpenRouter({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!!!!');
@@ -13,6 +21,38 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/greet', (req: Request, res: Response) => {
   res.json({ message: 'Hello from the server!' });
+});
+
+const chatSchema = z.object({
+  prompt: z
+    .string()
+    .trim()
+    .min(1, 'Prompt cannot be empty')
+    .max(1000, 'Prompt cannot exceed 1000 characters'),
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+  const parseResult = chatSchema.safeParse(req.body);
+  if (!parseResult.success) {
+    return res.status(400).json({ errors: parseResult.error.format() });
+  }
+  const { prompt } = req.body;
+
+  const response = await openrouter.chat.send({
+    chatRequest: {
+      model: 'openrouter/free',
+      messages: [
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+      stream: false,
+    },
+  });
+  res.json({
+    response: (response as ChatResult)?.choices?.[0]?.message?.content,
+  });
 });
 
 app.listen(port, () => {
